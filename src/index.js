e6963593fadb00a2ff6eaf0df30499dd802df73d
Denis <wolf-den@tut.by>
2021-08-29 21:48:48 +0300

    add store without redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello Redux');
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INC':
+            return state + 1;
+        case 'DEC':
+            return state - 1;
+        default:
+            return 0;
+    }
+}
+
+let state = reducer(undefined, {});
+state = reducer(state, {type: 'INC'});
+console.log(state);
+state = reducer(state, {type: 'INC'});
+console.log(state);

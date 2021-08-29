fc6a5803e69abf6ca2603861dd6ea6140efbdb08
Denis <wolf-den@tut.by>
2021-08-29 22:18:24 +0300

    add dispatch and subscribe

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import {createStore} from "redux";
+
 const reducer = (state = 0, action) => {
     switch (action.type) {
         case 'INC':
@@ -9,8 +11,21 @@ const reducer = (state = 0, action) => {
     }
 }
 
-let state = reducer(undefined, {});
-state = reducer(state, {type: 'INC'});
-console.log(state);
-state = reducer(state, {type: 'INC'});
-console.log(state);
+const store = createStore(reducer);
+console.log(store.getState());
+
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+
+
+
+// let state = reducer(undefined, {});
+// state = reducer(state, {type: 'INC'});
+// console.log(state);
+// state = reducer(state, {type: 'INC'});
+// console.log(state);

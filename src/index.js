0419b9a8be46ae43d2032a0a8e8c092e48bdd202
Den <wolf-den@tut.by>
2021-08-30 17:52:37 +0300

    feat: create counter component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
 import * as actions from './actions';
+import Counter from './counter';
 
 const store = createStore(reducer);
 const { dispatch } = store;
 
 const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
-document.getElementById('inc').addEventListener('click', inc);
-
-document.getElementById('dec').addEventListener('click', dec);
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-});
-
 const update = () => {
-  document.getElementById('counter').textContent = store.getState();
+  ReactDOM.render(
+    <Counter
+      counter={store.getState()}
+      inc={inc}
+      dec={dec}
+      rnd={() => {
+        const value = Math.floor(Math.random() * 10);
+        rnd(value);
+      }}
+    />,
+    document.getElementById('root')
+  );
 };
+update();
 
 store.subscribe(update);
